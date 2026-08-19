d6a6e619f6890e1f3c11e13bbe170cb2ebce8727
Anil bhusal <3nullroot@gmail.com>
2026-08-19 12:21:08 +0545

    registration.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello, World!";
+
+
+
+?>
+<h1>i am a php developer</h1>
+
+
+
+<a href="registation.php">Yha jau kta ho</a>
---
+++ registation.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="registation" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        p{
+            color: red;
+        }
+</head>
+<body>
+<h1>Registration form</h1>
+<p>Registration Form</p>
+<p>second line</p>
+    
+</body>
+</html>
